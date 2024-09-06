979410fbb6c541d991e16bdae1aeebd87c66dae7
Matheus <matheus271112@gmail.com>
2024-09-06 00:20:19 -0300

    Header update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('.header');
     const sobreSection = document.querySelector('.sobre');
     let lastScrollY = window.scrollY;
+    const menuButton = document.querySelector('.header__responsive__menu');
+    const menu = document.querySelector('.header__list');
+
+    menuButton.addEventListener('click', function () {
+        menu.classList.toggle('header__list--active')
+    })
 
     window.addEventListener('scroll', function () {
         const currentScrollY = window.scrollY;
         const sobreTop = sobreSection.getBoundingClientRect().top;
 
-        // Verifica se estamos na seção 'sobre' e rolando para baixo
         if (currentScrollY > lastScrollY && sobreTop <= 0) {
             header.classList.add('header__active');
-        } 
+        }
 
         else if (currentScrollY < lastScrollY || sobreTop > 0) {
             header.classList.remove('header__active');
         }
 
-        
+
         lastScrollY = currentScrollY;
     });
 });
